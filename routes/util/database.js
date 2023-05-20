804b4c336ba2a005541faea2f9b8a05090fd7e15
DiptenduMondal28 <diptendumondal.2801@gmail.com>
2023-05-20 18:42:30 +0530

    my sql operation

--- routes/util/database.js
+++ routes/util/database.js
@@ -1,10 +1,10 @@
-const mysql=require('mysql2');
+const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-complete',
-    password:'diptendu28'
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'diptendu28'
 });
 
-module.exports=pool.promise();
+module.exports = pool.promise();
