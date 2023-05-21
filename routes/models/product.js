c212b055b5b6bb6c62128314936cb385f60c5da3
DiptenduMondal28 <diptendumondal.2801@gmail.com>
2023-05-21 10:43:50 +0530

    sequilizer

--- routes/models/product.js
+++ routes/models/product.js
@@ -1,31 +1,31 @@
-const db = require('../util/database');
+const Sequelize=require('sequelize');
 
-const Cart = require('./cart');
+const sequelize=require('../util/database');
+const { DOUBLE } = require('mysql2/lib/constants/types');
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
+const Product=sequelize.define('product',{
+    id:{
+        type:Sequelize.INTEGER,
+        autoIncrement:true,
+        allowNull:false,
+        primaryKey:true
+    },
+    title:{
+        type:Sequelize.STRING,
+        allowNull:false
+    },
+    price:{
+        type: Sequelize.DOUBLE,
+        allowNull:false
+    },
+    imageUrl:{
+        type:Sequelize.STRING,
+        allowNull:false,
+    },
+    description:{
+        type:Sequelize.STRING,
+        allowNull:false
+    }
+});
 
-  save() {
-    return db.execute('INSERT INTO products(title,price,imageUrl,description) VALUES (?,?,?,?)',
-    [this.title,this.price,this.imageUrl,this.description]
-    );
-  }
-
-  static deleteById() {
-    return db.execute('DELETE FROM products WHERE id==this.id')
-  }
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM products');
-  }
-
-  static findById(id) {
-    
-  }
-};
+module.exports=Product;
