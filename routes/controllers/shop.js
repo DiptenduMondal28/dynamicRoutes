baf9cdc180ac1ce885ab129ad95aa1fadd3f1aed
DiptenduMondal28 <diptendumondal.2801@gmail.com>
2023-05-22 08:54:26 +0530

    delete

--- routes/controllers/shop.js
+++ routes/controllers/shop.js
@@ -2,39 +2,43 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
-      res.render('shop/product-list', {
-        prods: rows,
-        pageTitle: 'All Products',
-        path: '/products'
-      });
-    })
-    .catch(err => console.log(err));
+  Product.findAll().then(products=>{
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products'
+    });
+  }).catch(err=>{console.log(err)});
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    res.render('shop/product-detail', {
-      product: product,
-      pageTitle: product.title,
-      path: '/products'
-    });
-  });
+  console.log(prodId);
+  console.log(Product.findByPk(prodId))
+  Product.findAll({where:{id:prodId}}).then(product=>{
+    res.render('shop/product-detail',{
+      product:product[0],
+      pageTitle:product[0].title,
+      path:'/products'
+    })
+  }).catch(err=>console.log(err));
+  // Product.findByPk(prodId).then(product=>{
+  //   res.render('shop/product-detail',{
+  //     product:product,
+  //     pageTitle:product.title,
+  //     path:'/products'
+  //   })
+  // })
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
-      console.log(rows);
-      res.render('shop/index', {
-        prods: rows,
-        pageTitle: 'Shop',
-        path: '/'
-      });
-    })
-    .catch(err => console.log(err));
+  Product.findAll().then(products=>{
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  }).catch(err=>{console.log(err)});
 };
 
 exports.getCart = (req, res, next) => {
